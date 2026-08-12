a287df06ab7f029d9ee1e95e6c8cdd8250eaddfe
yk <114039324+YKL17@users.noreply.github.com>
2026-08-11 20:59:07 -0400

    feat: load preparation economy rebalance

--- vite.config.ts
+++ vite.config.ts
@@ -58,6 +58,7 @@ export default defineConfig(({ mode }) => {
             { tag: 'script', attrs: { src: './annual-rating-v10.js' }, injectTo: 'body' },
             { tag: 'script', attrs: { src: './single-event-result-v11.js' }, injectTo: 'body' },
             { tag: 'script', attrs: { src: './prep-state-fix-v12.js' }, injectTo: 'body' },
+            { tag: 'script', attrs: { src: './economy-prep-v13.js' }, injectTo: 'body' },
           ];
         },
       },
