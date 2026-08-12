1950e1c3810a6b6a4eba26239b3693e16db96b83
yk <114039324+YKL17@users.noreply.github.com>
2026-08-11 21:11:54 -0400

    feat: load annual retirement choice

--- vite.config.ts
+++ vite.config.ts
@@ -60,6 +60,7 @@ export default defineConfig(({ mode }) => {
             { tag: 'script', attrs: { src: './prep-state-fix-v12.js' }, injectTo: 'body' },
             { tag: 'script', attrs: { src: './economy-prep-v13.js' }, injectTo: 'body' },
             { tag: 'script', attrs: { src: './career-expansion-v14.js' }, injectTo: 'body' },
+            { tag: 'script', attrs: { src: './retirement-choice-v15.js' }, injectTo: 'body' },
           ];
         },
       },
