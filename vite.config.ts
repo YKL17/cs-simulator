fc506acc1c79e81395325a24253c837717a1f6f7
yk <114039324+YKL17@users.noreply.github.com>
2026-08-11 20:45:32 -0400

    fix: load single event result guard

--- vite.config.ts
+++ vite.config.ts
@@ -56,6 +56,7 @@ export default defineConfig(({ mode }) => {
             { tag: 'script', attrs: { src: './meta-system.js' }, injectTo: 'body' },
             { tag: 'script', attrs: { src: './prep-name-fix-v9.js' }, injectTo: 'body' },
             { tag: 'script', attrs: { src: './annual-rating-v10.js' }, injectTo: 'body' },
+            { tag: 'script', attrs: { src: './single-event-result-v11.js' }, injectTo: 'body' },
           ];
         },
       },
