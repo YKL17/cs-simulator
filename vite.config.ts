3a38f872eaea583e025fc984ecb55a5b1662e46c
yk <114039324+YKL17@users.noreply.github.com>
2026-08-21 16:26:04 -0400

    feat: load extended v2.20 translations

--- vite.config.ts
+++ vite.config.ts
@@ -66,6 +66,7 @@ export default defineConfig(({ mode }) => {
             { tag: 'script', attrs: { src: './ranking-progression-v18.js' }, injectTo: 'body' },
             { tag: 'script', attrs: { src: './ranking-season-decay-v19.js' }, injectTo: 'body' },
             { tag: 'script', attrs: { src: './i18n-v20.js' }, injectTo: 'body' },
+            { tag: 'script', attrs: { src: './i18n-v20-supplement.js' }, injectTo: 'body' },
           ];
         },
       },
